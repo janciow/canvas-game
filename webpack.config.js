7ddc703a79f5f2ccfd0c56083b275c6804567198
Jan Bancarewicz <jan.bancarewicz@gmail.com>
2025-11-11 22:30:37 +0100

    webpak

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,26 @@ module.exports = {
   entry: ['./src/js/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.js'
+    filename: 'js/bundle.js',
+    clean: true
   },
   devServer: {
-    contentBase: './dist'
+    static: './dist',
+    port: 8080,
+    open: true
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html'
     })
-  ]
-}
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+};
